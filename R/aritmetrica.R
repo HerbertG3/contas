77020a22d57f1bd1d753b476dbb5231a1544505b
Herbert Santos <hlrs1@discente.ifpe.edu.br>
2021-02-11 21:47:37 -0300

    first commit

---
+++ R/aritmetrica.R
@@ -0,0 +1,25 @@
+#' function to sum
+#'
+#'this function is good to sum.
+#'
+#' @param a A number.
+#' @param b It is another number.
+#'
+#'
+#' @export
+somar <- function(a,b){
+  sum(a,b)
+}
+
+#' function to subtract
+#'
+#'this function is good to subtract.
+#'
+#' @param a A number.
+#' @param b It is another number.
+#'
+#'
+#' @export
+subtrair<- function(a,b){
+  a-b
+}
